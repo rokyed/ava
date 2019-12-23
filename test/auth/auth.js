db8f37c5ab6fb790cd8411c172448ce468a5f61b
Andrei Bazavan <rokyed@gmail.com>
2019-12-23 18:10:46 -0500

    test fixed

--- test/auth/auth.js
+++ test/auth/auth.js
@@ -4,43 +4,35 @@ const {
 	TEST_WITH_ASYNC = 0
 } = process.env
 
+async function testAuth(c, li = '') {
+	await authScenarios.userRegister(c, li)
+	await authScenarios.userLogin(c, li)
+	await authScenarios.userGetsUserInfo(c, li)
+	await authScenarios.userChangesUserInfo(c, li)
+	await authScenarios.userTriesToDoActionWithLogin(c, li)
+	await authScenarios.userTriesToDoActionWithoutLogin(c, li)
+	await authScenarios.userChangesPassword(c, li)
+	await authScenarios.loginWrongUsernameOrPassword(c, li)
+	return true
+}
+
 module.exports = {
-	test: async function(c) {
-		await authScenarios.userRegister(c)
-		await authScenarios.userLogin(c)
-		await authScenarios.userGetsUserInfo(c)
-		await authScenarios.userChangesUserInfo(c)
-		await authScenarios.userTriesToDoActionWithLogin(c)
-		await authScenarios.userTriesToDoActionWithoutLogin(c)
-		await authScenarios.userChangesPassword(c)
-		await authScenarios.loginWrongUsernameOrPassword(c)
-	},
+	test: testAuth,
 
 	bashTest: async function (c, iterations) {
 		if (+TEST_WITH_ASYNC) {
 			for (let i = 0; i < iterations; i++) {
 				let li = (i + 1) + ''
-				await authScenarios.userRegister(c, li)
-				await authScenarios.userLogin(c, li)
-				await authScenarios.userGetsUserInfo(c, li)
-				await authScenarios.userChangesUserInfo(c, li)
-				await authScenarios.userTriesToDoActionWithLogin(c, li)
-				// await authScenarios.userTriesToDoActionWithoutLogin(c, li)
-				await authScenarios.userChangesPassword(c, li)
-				// await authScenarios.loginWrongUsernameOrPassword(c, li)
+				testAuth(c, li)
 			}
 		} else {
 			let promises = []
 			for (let i = 0; i < iterations; i++) {
 				let li = (i + 1) + ''
-				promises.push(authScenarios.userRegister(c, li))
+				promises.push(testAuth(c, li))
 			}
 
 			Promise.all(promises).then(async () => {
-				for (let i = 0; i < iterations; i++) {
-					let li = (i + 1) + ''
-					authScenarios.userLogin(c, li)
-				}
 				process.exit(0)
 			})
 		}
